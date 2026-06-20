21f81b5712f0619c01990fcdeea4e934ae9bf82a
Nikhil <nikhilprakash9005@gmail.com>
2026-06-21 02:43:41 +0530

    fix CORS

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:4000', 'https://packkart.in', 'https://www.packkart.in'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
